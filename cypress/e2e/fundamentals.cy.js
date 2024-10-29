f4c60f252915d0df7c1c19c5ea7ae7db574987e6
Musamasaba98 <masamata98@gmail.com>
2024-10-29 14:33:54 +0300

    Add more assertiveness so you perform action on the right item

--- cypress/e2e/fundamentals.cy.js
+++ cypress/e2e/fundamentals.cy.js
@@ -9,4 +9,18 @@ describe("Fundamentals tests", () => {
       "Testing Fundamentals"
     );
   });
+  it("Accordion works correctly", () => {
+    cy.visit("/fundamentals");
+    cy.contains(/Your tests will exist in a describe block/i).should(
+      "not.be.visible"
+    );
+    cy.get('[data-test="accordion-item-1"] div[role="button"]').click();
+    cy.contains(/Your tests will exist in a describe block/i).should(
+      "be.visible"
+    );
+    cy.get('[data-test="accordion-item-1"] div[role="button"]').click();
+    cy.contains(/Your tests will exist in a describe block/i).should(
+      "not.be.visible"
+    );
+  });
 });
